204c971731fd7553c9c88c5d4a54e35c4f0313e2
YOhonour <1083775096@qq.com>
2021-12-28 11:11:39 +0800

    4

--- test.c
+++ test.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <sys/socket.h> 
 #include "khook/engine.h"
 int main(){
     int a = 0;
     printf("Hello world!\n==========================================\n");
     a++;
+    123;
     return a;
 }
