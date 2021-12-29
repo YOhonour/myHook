82da3569e0c0524f7a20bfa80b74558c52feb08f
YOhonour <1083775096@qq.com>
2021-12-29 23:47:04 +0800

    7

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 
+#include <linux/string.h>
+
 #include "khook/engine.c"
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -20,13 +22,38 @@
 // }
 
 #include <linux/netdevice.h>
-
+#define VNI_HEAD 0x01020901
+// 1921的大端表示，linux主机字节序和网络字节序都是大端,也即 1->9->2->1
+#define size_t u32
 KHOOK(dev_queue_xmit);
+char ThisRunningFlag = 0;
 static int khook_dev_queue_xmit(struct sk_buff *skb)
 {
+	
 	int ret = 0;
+	unsigned int skbLen = skb->len;
+	printk("ljh msg: hook begin\n");
+	// if(ThisRunningFlag==1){//额外代码之执行一次
+	// 	u32 skbLen = skb->len;
+	// 	char* buf = (char*)malloc(sizeof(char)*(skbLen));
+	// 	memcpy(buf,skb->head,skbLen);
+	// 	size_t i = 0;
+	// 	printk("ljh\n");
+	// 	printk("截获数据:\n");
+	// 	for (i = 0; i < skbLen; i++)
+	// 	{
+	// 		printk("%0X,",buf[i]);
+
+	// 	}
+	// 	printk("输出完毕!\n");
+	// 	ThisRunningFlag=0;
+
+	// 	ret = KHOOK_ORIGIN(dev_queue_xmit, skb);
+	// 	printk("ljh msg :%s(%p) = %d,skb->len:%d\n", __func__, skb, ret,skbLen);
+	// }
 	ret = KHOOK_ORIGIN(dev_queue_xmit, skb);
-	printk("ljh msg :%s(%p) = %d\n", __func__, skb, ret);
+	// printk("ljh msg :%s(%p) = %d\n", __func__, skb, ret);
+	printk("ljh msg :%s(%p) = %d,skb->len:%d\n", __func__, skb, ret,skbLen);
 	return ret;
 }
 
--- test.c
+++ test.c
@@ -1,10 +1,43 @@
 #include <stdio.h>
 #include <sys/socket.h> 
+#include <string.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
 #include "khook/engine.h"
+#define VNI_HEAD_INT 17367553
+#define VNI_HEAD_HEX 0x01090201
+#define ETH_ALEN 6 //以太网地址长度
+typedef struct ethhdr {
+	unsigned char	h_dest[ETH_ALEN];	/* 目的地址	*/
+	unsigned char	h_source[ETH_ALEN];	/* 源地址   */
+	unsigned short	h_proto;            /* 类型：0x0800 IP数据报、0x0806 ARP请求/应答、0x8035 RARP请求/应答*/
+} ETHHDR;
+
 int main(){
-    int a = 0;
-    printf("Hello world!\n==========================================\n");
+    int a = 0x01020901;// 1921的大端表示，linux主机字节序和网络字节序都是大端有也即 1->9->2->1
+    long b = 1921;
+    char c[4] = {0};
+    memcpy(c,&a,sizeof(a));
+    printf("c[0]=%d,c[1]=%d,c[2]=%d,c[3]=%d\n",c[0],c[1],c[2],c[3]);
+    htonl(a);
+    memcpy(c,&a,sizeof(a));
+    printf("转换为网络字序后:\n");
+    printf("c[0]=%d,c[1]=%d,c[2]=%d,c[3]=%d\n",c[0],c[1],c[2],c[3]);
+    ntohl(a);
+    memcpy(c,&a,sizeof(a));
+    printf("转换为主机字序后:\n");
+    printf("c[0]=%d,c[1]=%d,c[2]=%d,c[3]=%d\n",c[0],c[1],c[2],c[3]);
+    if(VNI_HEAD_HEX==VNI_HEAD_INT){
+        printf("VNI_HEAD_HEX = VNI_HEAD_INT\n");
+    }else{
+        printf("VNI_HEAD_HEX != VNI_HEAD_INT\n");
+    }
+    printf("%d\n",sizeof(VNI_HEAD_HEX));
+    printf("a=%d\n",a);
+    printf("sizeof(int)=%d\n",sizeof(int));
+    printf("sizeof(VNI_HEAD_HEX)=%d\n",a);
+
+    printf("%d\n",sizeof(b));
     a++;
-    p2rin23tf1(s);
     return a;
 }
